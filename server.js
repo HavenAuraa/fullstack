7fed6c133997d79f958f1ad76add11d07903d89d
Sooriyaa <sooriyaa4@gmail.com>
2025-01-23 16:06:58 +0530

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const http = require('http');
+import express from 'express';
+
+const app = express();
+
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('<form action="/submit" method="POST"><label for="name">Name: </label><input type="text" id="name" name="name" required /><br><label for="email">Email: </label><input type="email" id="email" name="email" required /><br><button type="submit">Submit</button></form>');
 });
 
-server.listen(PORT, () => {
+app.post('/submit', (req, res) => {
+    const { name, email } = req.body;
+    res.send(`<h1>Thank you, ${name}!</h1><p>Your email is ${email}</p>`);
+});
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
